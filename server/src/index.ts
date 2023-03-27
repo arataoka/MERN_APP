36f90a6e94a6f46a70487d21a43023c1dd256fc4
arataoka <arata.okayasu@gmail.com>
2023-03-27 20:03:21 +0900

    feat: connect mongo DB

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,19 @@
 import express from "express";
+import * as mongoose from "mongoose";
+import * as dotenv from 'dotenv'
 
 const PORT = 8000
 const app = express();
+
+dotenv.config()
+
+const MONGO_DB_URL = process.env.MONGO_DB_URL || "";
+try {
+mongoose.connect(MONGO_DB_URL)
+    console.log("DB 接続中")
+} catch (e){
+    console.log(e)
+}
 
 app.listen(PORT,()=>{
     console.log(`http://localhost:${PORT}`)
---
+++ server/src/v1/models/user.ts
@@ -0,0 +1,15 @@
+import * as mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+})
+
+export const user = mongoose.model("user", userSchema)
